76a1be4aea3b1bcd5528e27e6c79e43634c5e789
Andres Martinez <andrepipemc@gmail.com>
2024-01-07 20:11:23 -0500

    feat: confirmed o canceled invitation

--- src/app/invite/[id]/actions.ts
+++ src/app/invite/[id]/actions.ts
@@ -1,10 +1,17 @@
-import { supabase } from "@fiwedding/lib/supabase";
+"use server";
+
 import { redirect } from "next/navigation";
 import z from "zod";
+import { zfd } from "zod-form-data";
+import { supabase } from "@fiwedding/lib/supabase";
 import { GuestType } from "./type";
 
 const schema = z.object({
   id: z.string().uuid(),
+});
+
+const schemaFormData = zfd.formData({
+  invited_id: z.string().uuid(),
 });
 
 export async function inviteValidate({
@@ -23,11 +30,44 @@ export async function inviteValidate({
   const { data, error } = await supabase
     .from("invited_guest")
     .select("id, first_name, last_name, email, confirmed, invited_group_id")
-    .or(`id.eq.${id},invited_group_id.eq.${id}`);
+    .or(`id.eq.${id}, invited_group_id.eq.${id}`);
 
   if (error) {
     redirect("/search-invite");
   }
 
   return data;
+}
+
+export async function inviteConfirm(formData: FormData) {
+  const validatedParams = schemaFormData.safeParse(formData);
+
+  if (!validatedParams.success) {
+    redirect("/search-invite");
+  }
+
+  const { data, error } = await supabase
+    .from("invited_guest")
+    .select("id, first_name, last_name, email, confirmed, invited_group_id")
+    .or(
+      `id.eq.${formData.get("invited_id")}, invited_group_id.eq.${formData.get(
+        "invited_id"
+      )}`
+    );
+
+  if (error) {
+    redirect("/search-invite");
+  }
+
+  const invited = formData.getAll("confirmed[]");
+  for (const guest of data) {
+    let confirmed = false;
+    if (invited.includes(guest.id)) {
+      confirmed = true;
+    }
+    await supabase
+      .from("invited_guest")
+      .update({ confirmed })
+      .eq("id", guest.id);
+  }
 }
